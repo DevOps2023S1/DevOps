bd580f53fecba56d59917b736521c461773e529b
Gastón Mateo Sanguinetti <gasmasan@gmail.com>
2023-07-08 16:07:51 -0300

    agregado de service de orders

--- terraform_files/terraform_ecs.tf
+++ terraform_files/terraform_ecs.tf
@@ -3,6 +3,15 @@ provider "aws" {
   region = "us-east-1" 
 }
 
+# Creación del cluster de ECS.
+# Este cluster tendra todos los servicios de produccion
+resource "aws_ecs_cluster" "production" {
+  name = "production"
+}
+
+########################################################
+################ microservicio PRODUCTS ################
+########################################################
 
 # Creación del grupo de seguridad
 resource "aws_security_group" "products_sg" {
@@ -47,11 +56,6 @@ resource "aws_ecs_task_definition" "products" {
       ]
     }
   ])
-}
-
-# Creación del cluster de ECS
-resource "aws_ecs_cluster" "production" {
-  name = "production"
 }
 
 # Creación del servicio de ECS
@@ -118,9 +122,129 @@ resource "aws_lb_target_group" "products" {
   }
 }
 
-
-
 # Salida con la URL del servicio
 output "products_service_url" {
   value = aws_lb.products.dns_name
+}
+
+
+########################################################
+################ microservicio ORDERS ################
+########################################################
+
+# Creación del grupo de seguridad
+resource "aws_security_group" "orders_sg" {
+  name        = "orders-sg"
+  description = "Security group for orders service"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+# Creación de la definición de la tarea de ECS
+resource "aws_ecs_task_definition" "orders" {
+  family                   = "orders"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = "arn:aws:iam::187585600197:role/LabRole"
+
+  cpu    = "256"  # Asigna la cantidad de CPU en milicore
+  memory = "512"  # Asigna la cantidad de memoria en MiB
+
+  container_definitions = jsonencode([
+    {
+      name          = "orders"
+      image         = "187585600197.dkr.ecr.us-east-1.amazonaws.com/orders-service:test-8" 
+      environment   = [
+        { "name": "APP_ARGS", "value": "http://paymetns-url http://shipping-url http://products-url" }
+      ]
+      portMappings = [
+        {
+          containerPort = 8080,
+          hostPort      = 8080,
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+# Creación del servicio de ECS
+resource "aws_ecs_service" "orders" {
+  name            = "orders"
+  cluster         = aws_ecs_cluster.production.id
+  task_definition = aws_ecs_task_definition.orders.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-036d2b225819bfb0a", "subnet-07dc0f066f2de4675"]
+    security_groups = [aws_security_group.orders_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.orders.arn
+    container_name   = "orders"
+    container_port   = 8080
+  }
+}
+
+# Creación del ALB (Application Load Balancer)
+resource "aws_lb" "orders" {
+  name               = "orders-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["subnet-036d2b225819bfb0a", "subnet-07dc0f066f2de4675"] # Reemplaza con los IDs de las subredes existentes de tu VPC
+
+  tags = {
+    Name = "orders-alb"
+  }
+}
+
+# Creación del listener del ALB
+resource "aws_lb_listener" "orders" {
+  load_balancer_arn = aws_lb.orders.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.orders.arn
+  }
+}
+
+# Creación del grupo de destino del ALB
+resource "aws_lb_target_group" "orders" {
+  name     = "orders-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = "vpc-0d93e90e894b1a396"  
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "404"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+# Salida con la URL del servicio
+output "orders_service_url" {
+  value = aws_lb.orders.dns_name
 }
