bedf32bb462357d9bb209d39a43907a4ee62b1c1
Gastón Mateo Sanguinetti <gasmasan@gmail.com>
2023-07-08 16:19:13 -0300

    agregar servicio shipping ecs

--- terraform_files/terraform_ecs.tf
+++ terraform_files/terraform_ecs.tf
@@ -248,3 +248,121 @@ resource "aws_lb_target_group" "orders" {
 output "orders_service_url" {
   value = aws_lb.orders.dns_name
 }
+
+########################################################
+################ microservicio SHIPPING ################
+########################################################
+
+# Creación del grupo de seguridad
+resource "aws_security_group" "shipping_sg" {
+  name        = "shipping-sg"
+  description = "Security group for shipping service"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+# Creación de la definición de la tarea de ECS
+resource "aws_ecs_task_definition" "shipping" {
+  family                   = "shipping"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = "arn:aws:iam::187585600197:role/LabRole"
+
+  cpu    = "256" 
+  memory = "512" 
+
+  container_definitions = jsonencode([
+    {
+      name          = "shipping"
+      image         = "187585600197.dkr.ecr.us-east-1.amazonaws.com/shipping-service:test-2"
+      portMappings = [
+        {
+          containerPort = 8080,
+          hostPort      = 8080,
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+# Creación del servicio de ECS
+resource "aws_ecs_service" "shipping" {
+  name            = "shipping"
+  cluster         = aws_ecs_cluster.production.id
+  task_definition = aws_ecs_task_definition.shipping.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-036d2b225819bfb0a", "subnet-07dc0f066f2de4675"]
+    security_groups = [aws_security_group.shipping_sg.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.shipping.arn
+    container_name   = "shipping"
+    container_port   = 8080
+  }
+}
+
+# Creación del ALB (Application Load Balancer)
+resource "aws_lb" "shipping" {
+  name               = "shipping-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = ["subnet-036d2b225819bfb0a", "subnet-07dc0f066f2de4675"]
+
+  tags = {
+    Name = "shipping-alb"
+  }
+}
+
+# Creación del listener del ALB
+resource "aws_lb_listener" "shipping" {
+  load_balancer_arn = aws_lb.shipping.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.shipping.arn
+  }
+}
+
+# Creación del grupo de destino del ALB
+resource "aws_lb_target_group" "shipping" {
+  name     = "shipping-target-group"
+  port     = 8080
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = "vpc-0d93e90e894b1a396"  
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "404"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+# Salida con la URL del servicio
+output "shipping_service_url" {
+  value = aws_lb.shipping.dns_name
+}
